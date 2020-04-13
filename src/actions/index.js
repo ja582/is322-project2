acc7f1581e4fe3149279e7f3495bad9640968e2e
thetarkus <mike@mikemob.com>
2020-04-13 05:02:39 -0400

    Add TaskStatuses object and setTask action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,14 @@
+export const TaskStatuses = {
+  toDo       : 'To Do',
+  inProgress : 'In Progress',
+  review     : 'Review',
+  done       : 'Done',
+};
+
+export const setTask = ({ id, status }) => ({
+  type: 'SET_TASK', id, status
+});
+
 export const addTask = ({ title, status }) => ({
   type: 'ADD_TASK', title, status
 });
