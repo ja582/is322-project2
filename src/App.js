f5aa29ce42efc01580d6dc5ba8ccb938f71240e0
thetarkus <mike@mikemob.com>
2020-04-13 05:04:11 -0400

    Move App to root

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { Route, Switch }   from 'react-router-dom';
+import './App.css';
+
+import Bar      from './components/Bar';
+import TaskAdd  from './containers/TaskAdd';
+import TaskGrid from './containers/TaskGrid';
+import TaskList from './containers/TaskList';
+
+function App() {
+  return (
+    <Fragment>
+      <Bar />
+      <main className="App">
+        <Switch>
+          <Route path='/'     component={TaskGrid} exact />
+          <Route path='/list' component={TaskList} />
+          <Route path='/add'  component={TaskAdd} />
+        </Switch>
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
