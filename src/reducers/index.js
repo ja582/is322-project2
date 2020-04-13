fdd0f7a8c8fb8c518c29c02a0d9ee5c4e08bf582
thetarkus <mike@mikemob.com>
2020-04-13 03:02:03 -0400

    Add reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,4 @@
 import { combineReducers }  from 'redux';
 import tasks                from './tasks';
-import visibilityFilter     from './visibilityFilter';
 
-export default combineReducers({
-  tasks,
-  visibilityFilter
-});
+export default combineReducers({ tasks });
