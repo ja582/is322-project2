0775276bc6237d4a29ba1d4baebcbb931b0ea6cb
thetarkus <mike@mikemob.com>
2020-04-12 23:32:49 -0400

    Add react-redux w/ tasks store, more organization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React               from 'react';
+import { render }          from 'react-dom';
+import { Provider }        from 'react-redux';
+import { createStore }     from 'redux';
+import rootReducer         from './reducers';
+import App                 from './components/App';
+import * as serviceWorker  from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
