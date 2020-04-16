021a4672cd4dbe43c9e3b7920ec61b2625ad69e2
thetarkus <mike@mikemob.com>
2020-04-15 23:46:25 -0400

    Form styling

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -4,32 +4,16 @@ import { Redirect } from "react-router-dom";
 import { TaskStatuses, TaskTypes } from '../actions';
 
 import {
-  Button, TextField, Select, MenuItem, Container
+  Typography, Button, TextField, Select, MenuItem, Container, Card, CardContent,
+  FormHelperText
 } from '@material-ui/core';
 
-import {
-  Close as CloseIcon
-} from '@material-ui/icons';
-
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& .MuiTextField-root': {
-            width: '25ch',
-            divider: {
-                margin: theme.spacing(2, 0),
-            },
-        },
-    },
-}));
 
 function TaskAdd({ defaults = {}, onSubmit, history }) {
   const [title, setTitle]       = React.useState(defaults.title  || '');
   const [status, setStatus]     = React.useState(defaults.status || 'toDo');
   const [type, setType]         = React.useState(defaults.status || 'task');
   const [complete, setComplete] = React.useState(false);
-  const classes = useStyles();
 
   const clickAdd = (event) => {
     if (typeof onSubmit != 'function')
@@ -41,39 +25,46 @@ function TaskAdd({ defaults = {}, onSubmit, history }) {
   };
 
   return (
-    <Container fixed>
-        <form className={classes.root} noValidate autoComplete="off">
-      { complete &&
-        <Redirect to={{
-          pathname : '/',
-          state    : { submit: true }
-        }} />
-      }
-      <div>
-        <TextField
-          label="Task Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div>
-        <Select value={status} onChange={(e) => setStatus(e.target.value)}>
-          {Object.entries(TaskStatuses).map(([key, value]) => (
-            <MenuItem value={key}>{value}</MenuItem>
-          ))}
-        </Select>
-      </div>
-      <div>
-        <Select value={type} onChange={(e) => setType(e.target.value)}>
-          {Object.entries(TaskTypes).map(([key, value]) => (
-            <MenuItem value={key}>{value}</MenuItem>
-          ))}
-        </Select>
-      </div>
-      <div>
-        <Button onClick={(e) => clickAdd(e)}>Add Task</Button>
-      </div>
-        </form>
+    <Container maxWidth="xs">
+      <Card style={{marginTop: '1rem'}}>
+        <CardContent>
+          <Typography variant="h6">New Task</Typography>
+          <form noValidate autoComplete="off">
+            { complete &&
+              <Redirect to={{
+                pathname : '/',
+                state    : { submit: true }
+              }} />
+            }
+            <div style={{marginTop: '1rem'}}>
+              <TextField
+                label="Task Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div style={{marginTop: '1rem'}}>
+              <FormHelperText>Task Status</FormHelperText>
+              <Select value={status} onChange={(e) => setStatus(e.target.value)}>
+                {Object.entries(TaskStatuses).map(([key, value]) => (
+                  <MenuItem value={key}>{value}</MenuItem>
+                ))}
+              </Select>
+            </div>
+            <div style={{marginTop: '1rem'}}>
+              <FormHelperText>Task Type</FormHelperText>
+              <Select value={type} onChange={(e) => setType(e.target.value)}>
+                {Object.entries(TaskTypes).map(([key, value]) => (
+                  <MenuItem value={key}>{value}</MenuItem>
+                ))}
+              </Select>
+            </div>
+            <div style={{marginTop: '1rem'}}>
+              <Button color="primary" onClick={(e) => clickAdd(e)}>Add Task</Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </Container>
   );
 }
