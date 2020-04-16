e0456a0e6b514a6b942029e5ed8684cda0783b9c
Joe <ja582@njit.edu>
2020-04-15 22:22:01 -0400

    Basic table sorting, tasks can now be sorted in various ways of your choosing.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {lighten, makeStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,6 +7,83 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import * as PropTypes from "prop-types";
+
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+    {id: 'id', numeric: true, label: 'ID Number'},
+    {id: 'name', numeric: false, label: 'Title'},
+    {id: 'status', numeric: false, label: 'Status'},
+];
+
+function Sorter(props) {
+    const {classes, order, orderBy, onRequestSort} = props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+return (
+    <TableHead>
+    <TableRow>
+        {headCells.map((headCell) => (
+            <TableCell
+                key={headCell.id}
+                padding={headCell.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === headCell.id ? order : false}
+            >
+                <TableSortLabel
+                    active={orderBy === headCell.id}
+                    direction={orderBy === headCell.id ? order : 'asc'}
+                    onClick={createSortHandler(headCell.id)}
+                >
+                    {headCell.label}
+                    {orderBy === headCell.id ? (
+                        <span className={classes.visuallyHidden}>
+        {
+            order === 'desc' ?
+                ' sorted descending ' : ' sorted ascending '
+        }
+      </span>) : null}
+                </TableSortLabel>
+            </TableCell>))}
+        </TableRow>
+    </TableHead>);
+}
+
+Sorter.propTypes = {
+    classes: PropTypes.object.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
 
 const useStyles = makeStyles({
     table: {
@@ -14,35 +91,50 @@ const useStyles = makeStyles({
     },
 });
 
-function TaskList({ tasks }) {
-    const classes = useStyles();
-  return (
-    <main className="TaskList">
-      <span>Task List</span>
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell><b>ID Number</b></TableCell>
-                        <TableCell align="right"><b>Title</b></TableCell>
-                        <TableCell align="right"><b>Status</b></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tasks.map((task) => (
-                        <TableRow key={task.id}>
-                            <TableCell component="th" scope="row">
-                                {task.id}
-                            </TableCell>
-                            <TableCell align="right">{task.title}</TableCell>
-                            <TableCell align="right">{task.status}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    </main>
-  );
+function TaskList({tasks}) {
+    {
+        const classes = useStyles();
+        const [order, setOrder] = React.useState('asc');
+        const [orderBy, setOrderBy] = React.useState('title');
+        const [page] = React.useState(0);
+        const [rowsPerPage] = React.useState(5);
+
+        const handleRequestSort = (event, property) => {
+            const isAsc = orderBy === property && order === 'asc';
+            setOrder(isAsc ? 'desc' : 'asc');
+            setOrderBy(property);
+        };
+
+        return (
+            <main className="TaskList">
+                <span>Task List</span>
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <Sorter
+                            classes={classes}
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            rowCount={tasks.length}
+                        />
+                        <TableBody>
+                            {stableSort(tasks, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((tasks, index) => {
+                                    return (
+                                        <TableRow>
+                                            <TableCell>{tasks.id}</TableCell>
+                                            <TableCell>{tasks.title}</TableCell>
+                                            <TableCell>{tasks.status}</TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </main>
+        );
+    }
 }
 
 export default TaskList;
